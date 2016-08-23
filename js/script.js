5810d15a4eb048de6772df8a4708717d856e5440
Dennis Kim <dennis.sk.kim@gmail.com>
2016-08-23 00:03:33 -0700

    added footer, and removedjquery class for type writer b/c too buggy

--- js/script.js
+++ js/script.js
@@ -1,20 +1,16 @@
-// (function($) {
-//     $.fn.typeAnimation = function(content) {
-//         deferred = jQuery.Deferred();
-//
-//         var contentArray = content.split(""),
-//             current = 0,
-//             elem = this;
-//             console.log(contentArray);
-//         setInterval(function() {
-//             if(current < contentArray.length) {
-//               elem.text(elem.text() + contentArray[current++]);
-//             }
-//         }, 100);
-//     };
-//
-// })(jQuery);
+(function($) {
+    $.fn.typeAnimation = function(content) {
+        deferred = jQuery.Deferred();
 
+        var contentArray = content.split(""),
+            current = 0,
+            elem = this;
+            console.log(contentArray);
+        setInterval(function() {
+            if(current < contentArray.length) {
+              elem.text(elem.text() + contentArray[current++]);
+            }
+        }, 100);
+    };
 
-$('.type-it').typeIt({
-});
+})(jQuery);
