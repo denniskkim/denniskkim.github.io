d6cfcaa9cf19dc4d4a3cb9ce2adad048174b8a28
Dennis Kim <dennis.sk.kim@gmail.com>
2017-01-10 11:03:51 -0800

    Added Project section

--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
-(function($) {
-    $.fn.typeAnimation = function(content) {
-        deferred = jQuery.Deferred();
+// (function($) {
+//     $.fn.typeAnimation = function(content) {
+//         deferred = jQuery.Deferred();
+//
+//         var contentArray = content.split(""),
+//             current = 0,
+//             elem = this;
+//             console.log(contentArray);
+//         setInterval(function() {
+//             if(current < contentArray.length) {
+//               elem.text(elem.text() + contentArray[current++]);
+//             }
+//         }, 100);
+//     };
+//
+// })(jQuery);
+var lineCounter = 0;
 
-        var contentArray = content.split(""),
-            current = 0,
-            elem = this;
-            console.log(contentArray);
-        setInterval(function() {
-            if(current < contentArray.length) {
-              elem.text(elem.text() + contentArray[current++]);
-            }
-        }, 100);
-    };
+$(document).ready(function(){
 
-})(jQuery);
+});
